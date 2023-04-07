906561376f4b877423c8ac9f0491adb2ab780731
Mariia Dryha <m.pishchanska@gmail.com>
2023-04-07 08:04:17 +0300

    fix:input text color

--- src/components/SignIn/styledComponentSignIn.ts
+++ src/components/SignIn/styledComponentSignIn.ts
@@ -3,24 +3,31 @@ import { TextField } from '@mui/material';
 
 export const WhiteTextField = styled(TextField)({
   '& label.Mui-focused': {
-    color: 'white',
+    color: '#fff',
   },
   '& .MuiFormHelperText-root ': {
     color: 'red',
   },
   '& .css-nz481w-MuiInputBase-input-MuiInput-input ': {
-    color: 'white',
+    color: '#fff',
   },
   '& .MuiInput-underline:after:invalid': {
-    borderBottomColor: 'white',
+    borderBottomColor: '#fff',
   },
   '& .MuiInput-underline:after': {
-    borderBottomColor: 'white',
+    borderBottomColor: '#fff',
   },
   '& .MuiInputLabel-root': {
-    color: 'white',
+    color: '#fff',
   },
   '& .MuiInput-underline::before': {
+    borderBottom: '2px solid #fff',
+  },
+  '& .css-2duac4': {
+    color: '#fff',
+  },
+  '& .MuiInputBase-input': {
+    color: '#fff',
     borderBottom: '2px solid #fff',
   },
 });
